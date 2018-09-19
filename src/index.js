1746f8d78f1c2f318fba3d1a5882b7f354fa5a4a
Jeong Min Lee <jacklee0223@gmail.com>
2018-09-19 08:25:05 -0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxPromise from 'redux-promise';
+import './index.css';
+import App from './components/app';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
