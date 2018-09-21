7d288c403720e4910e2a132cd0ce3adfdc8ecf24
Jeong Min Lee <jacklee0223@gmail.com>
2018-09-21 03:48:52 -0700

    add firebase

---
+++ src/components/main_container.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+export default class MainContainer extends Component {
+  render() {
+    return (
+      <div className="main-container">
+      </div>
+    );
+  }
+}
